8690d732d63f10d7b38562aa9cc57c38009f1a3f
김다은 <150661115+dani1552@users.noreply.github.com>
2024-03-25 17:30:23 +0900

    fix : stop button 수정

--- lib/features/game_select/widgets/stop_button.dart
+++ lib/features/game_select/widgets/stop_button.dart
@@ -5,15 +5,17 @@ class StopButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        print('Stop button pressed');
-      },
-      style: ElevatedButton.styleFrom(
-        foregroundColor: Colors.black,
-        backgroundColor: const Color(0xfff0e5c8), // 텍스트 색상을 흰색으로 변경
+    return Container(
+      child: ElevatedButton(
+        onPressed: () {
+          print('Stop button pressed');
+        },
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.black,
+          backgroundColor: const Color(0xfff0e5c8), // 텍스트 색상을 흰색으로 변경
+        ),
+        child: const Text('그만하기'),
       ),
-      child: const Text('그만하기'),
     );
   }
 }
