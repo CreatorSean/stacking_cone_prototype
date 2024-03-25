b124cb22db06bedfe68ac736a38d3fbe9d7552df
김다은 <150661115+dani1552@users.noreply.github.com>
2024-03-25 14:44:24 +0900

    feat : con container widget UI

---
+++ lib/features/game/widgets/cone_container_widget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ConContainerWidget extends StatelessWidget {
+  const ConContainerWidget({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            width: 360,
+            height: 300,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: const Color(0xFF332F23), // 전체 테두리 색상
+                width: 1.5, // 전체 테두리 두께
+              ),
+            ),
+            child: GridView.count(
+              crossAxisCount: 3,
+              childAspectRatio: 0.8,
+              children: List.generate(6, (index) {
+                return Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xfff0e5c8),
+                    border: Border.all(
+                      color: const Color(0xFF332F23),
+                      width: 1.5,
+                    ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      '$index',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                  ),
+                );
+              }),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
