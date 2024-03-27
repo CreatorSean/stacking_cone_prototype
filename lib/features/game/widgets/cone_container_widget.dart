930c9393d7d5497119b2d9345de667bf270fd6bc
yujinhappya <yujinhappya@naver.com>
2024-03-27 17:02:50 +0900

    Backgroundcolor and Image

--- lib/features/game/widgets/cone_container_widget.dart
+++ lib/features/game/widgets/cone_container_widget.dart
@@ -2,7 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ConContainerWidget extends StatelessWidget {
-  const ConContainerWidget({super.key});
+  const ConContainerWidget({Key? key});
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -19,21 +20,61 @@ class ConContainerWidget extends StatelessWidget {
           crossAxisCount: 3,
           childAspectRatio: 0.8,
           children: List.generate(6, (index) {
-            return Container(
-              decoration: BoxDecoration(
-                color: const Color(0xfff0e5c8),
-                border: Border.all(
-                  color: const Color(0xFF332F23),
-                  width: 1.5,
+            Widget childWidget;
+            if (index == 1) {
+              // For index 1, keep the background white
+              childWidget = Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    color: const Color(0xFF332F23),
+                    width: 1.5,
+                  ),
                 ),
-              ),
-              child: Center(
-                child: Text(
-                  '$index',
-                  style: Theme.of(context).textTheme.headlineSmall,
+                child: Center(
+                  child: Text(
+                    '$index',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
                 ),
-              ),
-            );
+              );
+            } else if (index == 3) {
+              // For index 3, display an image at the center
+              childWidget = Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xfff0e5c8),
+                  border: Border.all(
+                    color: const Color(0xFF332F23),
+                    width: 1.5,
+                  ),
+                ),
+                child: Center(
+                  child: Image.asset(
+                    'assets/images/redcone.png',
+                    width: 100, // Set width as desired
+                    height: 100, // Set height as desired
+                  ),
+                ),
+              );
+            } else {
+              // For other indexes, display a text
+              childWidget = Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xfff0e5c8),
+                  border: Border.all(
+                    color: const Color(0xFF332F23),
+                    width: 1.5,
+                  ),
+                ),
+                child: Center(
+                  child: Text(
+                    '$index',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                ),
+              );
+            }
+            return childWidget;
           }),
         ),
       ),
