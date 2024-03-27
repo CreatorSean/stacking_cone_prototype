a1196b5c77fe680e950af0b30599d91136a643fa
yujinhappya <yujinhappya@naver.com>
2024-03-27 20:32:10 +0900

    Cone Increase
    콘 쌓기 모드에서 하얀 불빛으로 쌓을 곳 알려주는 것과 콘이 그 위치에 놓여있는 거 구현

--- lib/features/game/widgets/cone_container_widget.dart
+++ lib/features/game/widgets/cone_container_widget.dart
@@ -21,42 +21,34 @@ class ConContainerWidget extends StatelessWidget {
           children: List.generate(6, (index) {
             Widget childWidget;
             if (index == 1) {
-              // For index 1, keep the background white
-              childWidget = Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(
-                    color: const Color(0xFF332F23),
-                    width: 1.5,
+              // For index 1, display image over white background
+              childWidget = Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: const Color(0xFF332F23),
+                        width: 1.5,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        '$index',
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                    ),
                   ),
-                ),
-                child: Center(
-                  child: Text(
-                    '$index',
-                    style: Theme.of(context).textTheme.headline6,
+                  Positioned.fill(
+                    child: Image.asset(
+                      'assets/images/redcone.png',
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ),
-              );
-            } else if (index == 3) {
-              // For index 3, display an image at the center
-              childWidget = Container(
-                decoration: BoxDecoration(
-                  color: const Color(0xfff0e5c8),
-                  border: Border.all(
-                    color: const Color(0xFF332F23),
-                    width: 1.5,
-                  ),
-                ),
-                child: Center(
-                  child: Image.asset(
-                    'assets/images/redcone.png',
-                    width: 100, // Set width as desired
-                    height: 100, // Set height as desired
-                  ),
-                ),
+                ],
               );
             } else {
-              // For other indexes, display a text
+              // For other indexes, display text only
               childWidget = Container(
                 decoration: BoxDecoration(
                   color: const Color(0xfff0e5c8),
